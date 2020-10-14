54697750722f2d009a437a379aa6acebc24c815a
Evan H L <ehutch7@gmail.com>
2020-10-14 16:51:01 -0400

    Adding renderable content to the Fib page.

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -26,4 +26,54 @@ export class Fib extends Component {
             seenIndexes: seenIndexes.data,
         });
     }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post("/api/values", {
+            index: this.state.index,
+        });
+
+        this.setState({ index: "" });
+    };
+
+    renderSeenIndexes() {
+        return this.state.seenIndexes.map(({ number }) => number).join(", ");
+    }
+
+    renderValues() {
+        const entries = Object.entries(this.state.values).map(
+            ([key, value]) => {
+                return (
+                    <div key={key}>
+                        For index {key} I calculated {value}
+                    </div>
+                );
+            }
+        );
+
+        return entries;
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Enter your Index:</label>
+                    <input
+                        value={this.state.index}
+                        onChange={(event) =>
+                            this.setState({ index: event.target.value })
+                        }
+                    />
+                    <button>Submit</button>
+                </form>
+
+                <h3>Indexes I have seen:</h3>
+                {this.renderSeenIndexes()}
+                <h3>Calculated Values:</h3>
+                {this.renderValues()}
+            </div>
+        );
+    }
 }
